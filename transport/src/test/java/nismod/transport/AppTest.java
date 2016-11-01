3fa7eee7e0cad57551d8235a9ef8c5d17b37057c
Milan Lovric <lovric.milan@gmail.com>
2016-11-01 15:27:26 +0000

    Initial commit for the NISMOD v2.0.0. transport model

---
+++ transport/src/test/java/nismod/transport/AppTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package nismod.transport;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for App
+ * @author Milan Lovric
+ *
+ */
+public class AppTest {
+
+	@Test
+	public void test() {
+		
+		assertTrue("This condition should be true", true);
+		
+	}
+
+}
