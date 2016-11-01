a8640047ec710dd868e48fc1ed0acfee0d318d78
Milan Lovric <lovric.milan@gmail.com>
2016-11-01 15:27:26 +0000

    Initial commit for the NISMOD v2.0.0. transport model

---
+++ transport/src/main/java/nismod/transport/App.java
@@ -0,0 +1,15 @@
+package nismod.transport;
+
+/**
+ * NISMOD V2.0.0 Transport Model
+ * @author Milan Lovric
+ *
+ */
+public class App {
+	
+    public static void main( String[] args ) {
+    	
+        System.out.println( "NISMOD V2.0.0 Transport Model" );
+        
+    }
+}
