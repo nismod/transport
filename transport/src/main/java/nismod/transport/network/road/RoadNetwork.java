5e908009368e2b9c200a474e6b6786251e485304
Milan Lovric <lovric.milan@gmail.com>
2016-11-11 13:50:30 +0000

    Directed graph representation of the road network

--- transport/src/main/java/nismod/transport/network/road/RoadNetwork.java
+++ transport/src/main/java/nismod/transport/network/road/RoadNetwork.java
@@ -6,9 +6,20 @@ package nismod.transport.network.road;
 import java.awt.Color;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Iterator;
 
 import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.graph.build.feature.FeatureGraphGenerator;
+import org.geotools.graph.build.line.BasicDirectedLineGraphBuilder;
+import org.geotools.graph.build.line.DirectedLineStringGraphGenerator;
+import org.geotools.graph.structure.DirectedEdge;
 import org.geotools.graph.structure.DirectedGraph;
+import org.geotools.graph.structure.DirectedNode;
+import org.geotools.graph.structure.Edge;
+import org.geotools.graph.structure.Graph;
+import org.geotools.graph.structure.Node;
 import org.geotools.map.FeatureLayer;
 import org.geotools.map.MapContent;
 import org.geotools.styling.PolygonSymbolizer;
@@ -16,9 +27,14 @@ import org.geotools.styling.SLD;
 import org.geotools.styling.Style;
 import org.geotools.styling.StyleBuilder;
 import org.geotools.swing.JMapFrame;
+import org.opengis.feature.simple.SimpleFeature;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.Point;
 
 /**
- * A routable road network built from the shapefiles
+ * A routable road network built from the shapefiles.
  * @author Milan Lovric
  *
  */
@@ -35,24 +51,27 @@ public class RoadNetwork {
 	 * @param networkUrl Url for the shapefile with road network
 	 * @param nodesUrl Url for the shapefile with nodes
 	 * @param AADFurl Url for the shapefile with AADF counts
+	 * @throws IOException 
 	 */
-	public RoadNetwork(URL zonesUrl, URL networkUrl, URL nodesUrl, URL AADFurl) {
+	public RoadNetwork(URL zonesUrl, URL networkUrl, URL nodesUrl, URL AADFurl) throws IOException {
 
 		zonesShapefile = new ShapefileDataStore(zonesUrl);
 		networkShapefile = new ShapefileDataStore(networkUrl);
 		nodesShapefile = new ShapefileDataStore(nodesUrl);
 		AADFshapefile = new ShapefileDataStore(AADFurl);
+
+		//build the graph
+		this.build();
 	}
 
 	/**
-	 * Visualises the road network as loaded from shapefiles
+	 * Visualises the road network as loaded from shapefiles.
 	 * @throws IOException
 	 */
 	public void visualise() throws IOException {
 
 		//create a map
 		MapContent map = new MapContent();
-
 		//set windows title
 		map.setTitle("Test Area");
 
@@ -68,7 +87,7 @@ public class RoadNetwork {
 
 		//create style for road network
 		Style networkStyle = SLD.createLineStyle(Color.GREEN, 2.0f, "CP_Number", null);
-		
+
 		//add network layer to the map
 		FeatureLayer networkLayer = new FeatureLayer(networkShapefile.getFeatureSource(), networkStyle);
 		map.addLayer(networkLayer);
@@ -82,12 +101,206 @@ public class RoadNetwork {
 
 		//create style for AADF counts
 		Style AADFstyle = SLD.createPointStyle("Circle", Color.DARK_GRAY, Color.YELLOW, 1, 4, "CP", null);
-		
+
 		//add counts layer to the map     
 		FeatureLayer AADFlayer = new FeatureLayer(AADFshapefile.getFeatureSource(), AADFstyle);
 		map.addLayer(AADFlayer);
 
 		//show the map 
 		JMapFrame.showMap(map);
+	}
+
+	/**
+	 * Getter method for the road network.
+	 * @return Directed graph representation of the road network.
+	 */
+	public DirectedGraph getNetwork() {
+		
+		return network;
+	}
+	
+	/**
+	 * Builds a directed graph representation of the road network
+	 * @throws IOException 
+	 */
+	private void build() throws IOException {
+
+		//get feature collections from the shapefiles
+		SimpleFeatureCollection networkFeatureCollection = networkShapefile.getFeatureSource().getFeatures();
+		SimpleFeatureCollection nodesFeatureCollection = nodesShapefile.getFeatureSource().getFeatures();
+		SimpleFeatureCollection AADFfeatureCollection = AADFshapefile.getFeatureSource().getFeatures();
+
+		//create undirected graph with features from the network shapefile
+		Graph undirectedGraph = createUndirectedGraph(networkFeatureCollection);
+
+		//graph builder to build a directed graph
+		BasicDirectedLineGraphBuilder graphBuilder = new BasicDirectedLineGraphBuilder();
+		
+		//create nodes of the directed graph directly from the nodes shapefile
+		addNodesToGraph(graphBuilder, nodesFeatureCollection);
+
+		//for each edge from the undirected graph create (usually) two directed edges in the directed graph
+		//and assign the data from the count points
+		addEdgesToGraph(graphBuilder, undirectedGraph, AADFfeatureCollection);
+
+		//set the instance field with the generated directed graph
+		this.network = (DirectedGraph) graphBuilder.getGraph();
+
+		System.out.println("Undirected graph representation of the road network:");
+		System.out.println(undirectedGraph);
+		System.out.println("Directed graph representation of the road network:");
+		System.out.println(network);
+	}
+
+	/**
+	 * Creates an undirected graph representation of the network from the feature collection.
+	 * @param featureCollection Feature collection describing the road network.
+	 * @return Undirected graph representation of the road network.
+	 */
+	private Graph createUndirectedGraph(SimpleFeatureCollection featureCollection) {
+
+		//create a linear graph generator
+		DirectedLineStringGraphGenerator lineStringGenerator = new DirectedLineStringGraphGenerator();
+		//wrap it in a feature graph generator
+		FeatureGraphGenerator featureGenerator = new FeatureGraphGenerator(lineStringGenerator);
+		//throw all the features into the graph generator
+		SimpleFeatureIterator iter = featureCollection.features();
+		try {
+			while(iter.hasNext()){
+				SimpleFeature feature = iter.next();
+				featureGenerator.add(feature);
+			}
+		} finally {
+			//feature iterator is a live connection that must be closed
+			iter.close();
+		}
+		//build the graph
+		Graph graph = featureGenerator.getGraph();
+		return graph;
+	}
+
+	/**
+	 * Builds the nodes of the directed graph.
+	 * @param graphBuilder Directed graph builder.
+	 * @param nodesFeatureCollection Feature collection with the nodes.
+	 */
+	private void addNodesToGraph(BasicDirectedLineGraphBuilder graphBuilder, SimpleFeatureCollection nodesFeatureCollection) {
+
+		//iterate through the nodes and add them to the directed graph
+		SimpleFeatureIterator iter = nodesFeatureCollection.features();
+		try {
+			while (iter.hasNext()) {
+				DirectedNode node = (DirectedNode) graphBuilder.buildNode();
+				SimpleFeature sf = iter.next();
+				node.setObject(sf);
+				//override internally assigned node IDs with the ones from the shapefile
+				node.setID(Math.toIntExact((long) sf.getAttribute("nodeID")));
+				graphBuilder.addNode(node);
+			} 
+		} finally {
+			//feature iterator is a live connection that must be closed
+			iter.close();
+		}
+	}
+
+	/** Builds the edges of the directed graph.
+	 * @param graphBuilder Directed graph builder.
+	 * @param sourceGraph Undirected graph.
+	 * @param AADFfeatureCollection Feature collection with count points.
+	 */
+	private void addEdgesToGraph(BasicDirectedLineGraphBuilder graphBuilder, Graph sourceGraph, SimpleFeatureCollection AADFfeatureCollection) {
+
+		DirectedGraph graphDestination = (DirectedGraph) graphBuilder.getGraph();
+
+		//iterate through the edges of the source graph
+		Iterator edgesIterator = sourceGraph.getEdges().iterator();
+		while (edgesIterator.hasNext()) {
+			Edge edge = (Edge) edgesIterator.next();
+			SimpleFeature edgeFeature = (SimpleFeature) edge.getObject();
+			MultiLineString mls = (MultiLineString) edgeFeature.getDefaultGeometry();
+			//System.out.println(edgeFeature.getDefaultGeometryProperty()); //very detailed
+			//System.out.println(mls.toText());
+			//System.out.println(mls.getGeometryN(0));
+			Coordinate c1 = mls.getCoordinates()[0];
+			Coordinate c2 = mls.getCoordinates()[1];
+
+			//iterate through the nodes of the destination graph to find the ones that equal the
+			//start and end coordinates of the edge of the source graph
+			Node nodeA = null;
+			Node nodeB = null;
+			Iterator nodesIterator = graphDestination.getNodes().iterator();
+			while (nodesIterator.hasNext() && (nodeA == null || nodeB == null)) {
+				Node node = (Node) nodesIterator.next();
+				SimpleFeature feat = (SimpleFeature) node.getObject();
+				Point point = (Point) feat.getDefaultGeometry();
+				Coordinate coord = point.getCoordinate();
+				if (c1.equals(coord)) nodeA = node;
+				if (c2.equals(coord)) nodeB = node;	
+			}
+			if (nodeA == null || nodeB == null) System.err.println("There is a road link for which start or end node is missing in the nodes shapefile.");
+
+			//add edge A->B or B->A or both (depending on the direction information contained in the AADF count points)
+			DirectedEdge directedEdge = null;
+			DirectedEdge directedEdge2 = null;
+			//iterate through AADFs to find counts with the same CP as the edge
+			SimpleFeatureIterator iterator = AADFfeatureCollection.features();
+			try {
+				while (iterator.hasNext()) {
+					SimpleFeature countFeature = iterator.next();
+					long cp =  (long) countFeature.getAttribute("CP");
+					double cn = (double) edgeFeature.getAttribute("CP_Number");
+					
+					//if count point matches the edge
+					if (cp == cn) {
+						//get the coordinates of the nodes
+						SimpleFeature featA = (SimpleFeature) nodeA.getObject();
+						SimpleFeature featB = (SimpleFeature) nodeB.getObject();
+						Point pointA = (Point) featA.getDefaultGeometry();
+						Point pointB = (Point) featB.getDefaultGeometry();
+						Coordinate coordA = pointA.getCoordinate(); 
+						Coordinate coordB = pointB.getCoordinate();
+
+						//get the direction information from the count point and create directed edge(s)
+						String direction = (String) countFeature.getAttribute("iDir");
+						char dir = direction.charAt(0);
+						switch (dir) {
+						case 'N': //North
+							if (coordB.y > coordA.y) directedEdge = (DirectedEdge) graphBuilder.buildEdge(nodeA, nodeB);
+							else					 directedEdge = (DirectedEdge) graphBuilder.buildEdge(nodeB, nodeA);
+							directedEdge.setObject(countFeature);
+							graphBuilder.addEdge(directedEdge);	break; 
+						case 'S': //South
+							if (coordB.y < coordA.y) directedEdge = (DirectedEdge) graphBuilder.buildEdge(nodeA, nodeB);
+							else					 directedEdge = (DirectedEdge) graphBuilder.buildEdge(nodeB, nodeA);
+							directedEdge.setObject(countFeature);
+							graphBuilder.addEdge(directedEdge); break; 
+						case 'W': //West
+							if (coordB.x < coordA.x) directedEdge = (DirectedEdge) graphBuilder.buildEdge(nodeA, nodeB);
+							else 					 directedEdge = (DirectedEdge) graphBuilder.buildEdge(nodeB, nodeA);
+							directedEdge.setObject(countFeature);
+							graphBuilder.addEdge(directedEdge); break; 
+						case 'E': //East
+							if (coordB.x > coordA.x) directedEdge = (DirectedEdge) graphBuilder.buildEdge(nodeA, nodeB);
+							else					 directedEdge = (DirectedEdge) graphBuilder.buildEdge(nodeB, nodeA);
+							directedEdge.setObject(countFeature);
+							graphBuilder.addEdge(directedEdge); break; 
+						case 'C': //Combined directions
+							directedEdge = (DirectedEdge) graphBuilder.buildEdge(nodeA, nodeB);
+							directedEdge.setObject(countFeature);
+							graphBuilder.addEdge(directedEdge);
+							directedEdge2 = (DirectedEdge) graphBuilder.buildEdge(nodeB, nodeA);
+							directedEdge2.setObject(countFeature); // add the same count point to the other direction
+							graphBuilder.addEdge(directedEdge2); break; 
+						default: System.err.println("Unrecognized direction character.");
+						}
+					}
+				} //while AADFs
+			}
+			finally {
+				iterator.close();
+			}
+			
+			if (directedEdge == null) System.err.println("No count point data found for this edge.");
+		} //while edges
 	}
 }
