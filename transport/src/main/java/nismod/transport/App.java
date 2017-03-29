3c91f4678e65707c60964aa65fe5c9f1d390211f
Tom Russell <tomalrussell@gmail.com>
2017-03-29 14:38:11 +0100

    wrapper: run from jar-with-dependencies

--- transport/src/main/java/nismod/transport/App.java
+++ transport/src/main/java/nismod/transport/App.java
@@ -18,51 +18,57 @@ import nismod.transport.network.road.RoadNetwork;
  *
  */
 public class App {
-	
-    public static void main( String[] args ) throws IOException {
-    	
-        System.out.println( "NISMOD V2.0.0 Transport Model" );
-        
-		final String areaCodeFileName = "./src/test/resources/testdata/nomisPopulation.csv";
-		final String areaCodeNearestNodeFile = "./src/test/resources/testdata/areaCodeToNearestNode.csv";
-		final String workplaceZoneFileName = "./src/test/resources/testdata/workplacePopulation.csv";
-		final String workplaceZoneNearestNodeFile = "./src/test/resources/testdata/workplaceZoneToNearestNode.csv";
 
-		final URL zonesUrl2 = new URL("file://src/test/resources/testdata/zones.shp");
-		final URL networkUrl2 = new URL("file://src/test/resources/testdata/network.shp");
-		final URL nodesUrl2 = new URL("file://src/test/resources/testdata/nodes.shp");
-		final URL AADFurl2 = new URL("file://src/test/resources/testdata/AADFdirected.shp");
-		
-		final String baseYearODMatrixFile = "./src/test/resources/testdata/passengerODM.csv";
-		final String baseYearFreightMatrixFile = "./src/test/resources/testdata/freightMatrix.csv";
-		final String populationFile = "./src/test/resources/testdata/population.csv";
-		final String GVAFile = "./src/test/resources/testdata/GVA.csv";
-		//final String energyUnitCostsFile = "./src/test/resources/testdata/energyUnitCosts.csv";
-		
-		final String roadExpansionFileName = "./src/test/resources/testdata/roadExpansion.properties";
-		final String vehicleElectrificationFileName = "./src/test/resources/testdata/vehicleEletrification.properties";		
-		
-		final String baseYear = args[0];
-		final String predictedYear = args[1];
-		final String energyUnitCostsFile = args[2];
-		final String outputFile = args[3];
-		
-		//create a road network
-		RoadNetwork roadNetwork2 = new RoadNetwork(zonesUrl2, networkUrl2, nodesUrl2, AADFurl2, areaCodeFileName, areaCodeNearestNodeFile, workplaceZoneFileName, workplaceZoneNearestNodeFile);
-				
-		//load interventions
-		List<Intervention> interventions = new ArrayList<Intervention>();
-		RoadExpansion re = new RoadExpansion(roadExpansionFileName);
-		VehicleElectrification ve = new VehicleElectrification(vehicleElectrificationFileName);
-		interventions.add(re);
-		interventions.add(ve);
-		
-		//the main demand model
-		DemandModel dm = new DemandModel(roadNetwork2, baseYearODMatrixFile, baseYearFreightMatrixFile, populationFile, GVAFile, energyUnitCostsFile, interventions);
-		
-		dm.predictHighwayDemand(Integer.parseInt(predictedYear), Integer.parseInt(baseYear));
-		
-		dm.saveEnergyConsumptions(Integer.parseInt(predictedYear), outputFile);
+    public static void main( String[] args ) throws IOException {
+
+        System.out.println( "NISMOD V2.0.0 Transport Model" );
+		try {
+			final String areaCodeFileName = "./src/test/resources/testdata/nomisPopulation.csv";
+			final String areaCodeNearestNodeFile = "./src/test/resources/testdata/areaCodeToNearestNode.csv";
+			final String workplaceZoneFileName = "./src/test/resources/testdata/workplacePopulation.csv";
+			final String workplaceZoneNearestNodeFile = "./src/test/resources/testdata/workplaceZoneToNearestNode.csv";
+
+			final URL zonesUrl2 = new URL("file://src/test/resources/testdata/zones.shp");
+			final URL networkUrl2 = new URL("file://src/test/resources/testdata/network.shp");
+			final URL nodesUrl2 = new URL("file://src/test/resources/testdata/nodes.shp");
+			final URL AADFurl2 = new URL("file://src/test/resources/testdata/AADFdirected.shp");
+
+			final String baseYearODMatrixFile = "./src/test/resources/testdata/passengerODM.csv";
+			final String baseYearFreightMatrixFile = "./src/test/resources/testdata/freightMatrix.csv";
+			final String populationFile = "./src/test/resources/testdata/population.csv";
+			final String GVAFile = "./src/test/resources/testdata/GVA.csv";
+			//final String energyUnitCostsFile = "./src/test/resources/testdata/energyUnitCosts.csv";
+
+			final String roadExpansionFileName = "./src/test/resources/testdata/roadExpansion.properties";
+			final String vehicleElectrificationFileName = "./src/test/resources/testdata/vehicleEletrification.properties";
+
+			final String baseYear = args[0];
+			final String predictedYear = args[1];
+			final String energyUnitCostsFile = args[2];
+			final String outputFile = args[3];
+
+			//create a road network
+			RoadNetwork roadNetwork2 = new RoadNetwork(zonesUrl2, networkUrl2, nodesUrl2, AADFurl2, areaCodeFileName, areaCodeNearestNodeFile, workplaceZoneFileName, workplaceZoneNearestNodeFile);
+
+			//load interventions
+			List<Intervention> interventions = new ArrayList<Intervention>();
+			RoadExpansion re = new RoadExpansion(roadExpansionFileName);
+			VehicleElectrification ve = new VehicleElectrification(vehicleElectrificationFileName);
+			interventions.add(re);
+			interventions.add(ve);
+
+			//the main demand model
+			DemandModel dm = new DemandModel(roadNetwork2, baseYearODMatrixFile, baseYearFreightMatrixFile, populationFile, GVAFile, energyUnitCostsFile, interventions);
+
+			dm.predictHighwayDemand(Integer.parseInt(predictedYear), Integer.parseInt(baseYear));
+
+			dm.saveEnergyConsumptions(Integer.parseInt(predictedYear), outputFile);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("Done");
+		}
     }
 }
 
