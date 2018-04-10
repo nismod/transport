ec76857b6bf276f598db66034c78022b63125ad8
Tom Russell <tomalrussell@gmail.com>
2018-04-10 11:30:14 +0100

    Mark GVA as TODO, remove OD-matrix reshaping

--- integration/run.py
+++ integration/run.py
@@ -127,22 +127,12 @@ class TransportWrapper(SectorModel):
             current_population = data_handle.get_data("population")[:,0]
             w.writerow((data_handle.current_timestep, ) + tuple(current_population))
 
+        # TODO base and current gva_per_head
         # GVAFile = GVA.csv
         # year,          E06000045,E07000086,E07000091,E06000046
         # base/current   [LAD,...]
         # 2015,          23535.00,27860.00,24418.00,17739.00
         # 2020,          23535.00,27860.00,24418.00,17739.00
-        with open('gva.csv' ,'w') as file_handle:
-            w = csv.writer(file_handle)
-
-            gva_region_names = self._input_region_names("population")
-            w.writerow(('year', ) + tuple(gva_region_names))
-
-            base_gva_per_capita = data_handle.get_base_timestep_data("GVA_per_capita")[:,0]
-            w.writerow((data_handle.base_timestep, ) + tuple(base_gva_per_capita))
-
-            current_gva_per_capita = data_handle.get_data("GVA_per_capita")[:,0]
-            w.writerow((data_handle.current_timestep, ) + tuple(current_gva_per_capita))
 
     def _set_outputs(self, data_handle, working_dir)::
         """Read results from model and write to data handle
@@ -156,7 +146,7 @@ class TransportWrapper(SectorModel):
             values = next(r)[1:]
             for fuel, val in zip(header, values):
                 data_handle.set_results(
-                    "energy_consumption_{}".format(fuel),
+                    "energy_consumption__{}".format(fuel.lower()),
                     np.array([[float(val)]])
                 )
 
@@ -164,61 +154,3 @@ class TransportWrapper(SectorModel):
         """Return value of objective function, to-be-defined
         """
         pass
-
-
-def od_array_to_matrix(data_array, od_region_names):
-    """Convert origin-destination data from a smif data array (assuming a single
-    interval) to a matrix form with origins as row headings, destinations as
-    column headings.
-
-    Parameters
-    ----------
-    data_array: numpy.ndarray
-        Data array of dimensions [number of o-d pairs] x [single interval]
-    od_region_names: list
-        list of o-d names, in "{origin_id}-{destination_id}" format
-
-    Returns
-    -------
-    data_matrix: pandas.DataFrame
-        data frame suitable for conversion straight to_csv
-    """
-    df = pd.DataFrame({
-        # assume OD regions are named "origin_id-destination_id"
-        "origin": [name.split("-")[0] for name in od_region_names],
-        "destination": [name.split("-")[1] for name in od_region_names],
-        # assume scenario data has single interval per region
-        # - i.e. array shape is like (300,1)
-        "val": data_array[:, 0]
-    })
-    # pivot destinations to become column headers
-    data_matrix = pd.pivot_table(df, values='val', index='origin', columns=['destination'])
-    return data_matrix
-
-
-def od_matrix_to_array(data_matrix, od_region_names):
-    """Convert origin-destination data from matrix form (with origins as row
-    headings, destinations as column headings) back to data array suitable
-    for smif outputs.
-
-    Parameters
-    ----------
-    data_matrix: pandas.DataFrame
-        data frame suitable for conversion straight to_csv
-    od_region_names: list
-        list of o-d names, in "{origin_id}-{destination_id}" format
-
-    Returns
-    -------
-    data_array: numpy.ndarray
-        Data array of dimensions [number of o-d pairs] x [single interval]
-    """
-    df = pd.melt(data_matrix, id_vars=['origin'])  # unpivot, retaining origin column
-    df['name'] = df['origin'] + "-" + df['variable']  # concatenate region name strings
-    del df['origin']
-    del df['variable']
-    ixd = df.set_index('name')  # create index on name
-    s = ixd.loc[od_region_names, 'value']  # lookup values using region names in smif order
-    n = s.as_matrix()  # convert series to ndarray
-    data_array = n.reshape(n.size,1)  # reshape to nested array for smif
-    return data_array
