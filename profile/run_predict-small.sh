1f25f8b449a3a17055b2111018986d197867f5d6
Tom Russell <tomalrussell@gmail.com>
2018-10-24 12:10:26 +0100

    Include non-safe points in small profile

--- profile/run_predict-small.sh
+++ profile/run_predict-small.sh
@@ -5,6 +5,7 @@ cd $script_dir/../transport && java \
   -XX:+UnlockCommercialFeatures \
   -XX:+FlightRecorder \
   -XX:FlightRecorderOptions=defaultrecording=true,dumponexit=true,settings=../profile/settings-small.jfc \
+  -XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints \
   -XX:MaxHeapSize=20g \
   -cp ./target/transport-0.0.1-SNAPSHOT-jar-with-dependencies.jar \
   nismod.transport.App \
