716d716b76c4cd307be6de7defe31f248cf29999
Tom Russell <tomalrussell@gmail.com>
2021-06-14 15:29:55 +0100

    Work with inputs-outputs results directory

--- dafni-run.sh
+++ dafni-run.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
-echo "$(date -I'seconds') Start dafni-run.sh" | tee -a /data/outputs/log.txt
+
+# Create results directory if not exists
+mkdir -p /data/inputs/results
+# Copy results from inputs to outputs
+# (outputs of previous steps turn up in inputs)
+mv /data/inputs/results /data/outputs/results
+
+echo "$(date -I'seconds') Start dafni-run.sh" | tee -a /data/outputs/results/log.txt
 
 # Run from data directory
 pushd /data
 
+
     # Check inputs and outputs
-    tree . | tee -a /data/outputs/log.txt
+    tree . | tee -a /data/outputs/results/log.txt
 
     # Run model
-    echo "$(date -I'seconds') java -cp /root/transport.jar nismod.transport.App $ARGS"  | tee -a /data/outputs/log.txt
-    java -cp /root/transport.jar nismod.transport.App $ARGS | tee -a /data/outputs/log.txt
+    echo "$(date -I'seconds') java -cp /root/transport.jar nismod.transport.App $ARGS"  | tee -a /data/outputs/results/log.txt
+    java -cp /root/transport.jar nismod.transport.App $ARGS | tee -a /data/outputs/results/log.txt
 
 popd
 
-echo "$(date -I'seconds') End dafni-run.sh" | tee -a /data/outputs/log.txt
+echo "$(date -I'seconds') End dafni-run.sh" | tee -a /data/outputs/results/log.txt
