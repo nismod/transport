f5bf4173a60293ab23495f556cbff8a27640c5af
Tom Russell <tomalrussell@gmail.com>
2021-06-15 17:36:17 +0100

    Check free memory

--- dafni-run.sh
+++ dafni-run.sh
@@ -8,17 +8,18 @@ mv /data/inputs/results /data/outputs/results
 
 echo "$(date -I'seconds') Start dafni-run.sh" | tee -a /data/outputs/results/log.txt
 
-java $JAVA_OPTS -XX:+PrintFlagsFinal -version | grep 'HeapSize' | tee -a /data/outputs/results/log.txt
+free -g | tee -a /data/outputs/results/log.txt
+
+java $JAVA_OPTS -XshowSettings:vm -version | tee -a /data/outputs/results/log.txt
 
 # Run from data directory
 pushd /data
-
 
     # Check inputs and outputs
     tree . | tee -a /data/outputs/results/log.txt
 
     # Run model
-    echo "$(date -I'seconds') java -cp /root/transport.jar nismod.transport.App $ARGS"  | tee -a /data/outputs/results/log.txt
+    echo "$(date -I'seconds') java $JAVA_OPTS -cp /root/transport.jar nismod.transport.App $ARGS"  | tee -a /data/outputs/results/log.txt
     java $JAVA_OPTS -cp /root/transport.jar nismod.transport.App $ARGS | tee -a /data/outputs/results/log.txt
 
 popd
